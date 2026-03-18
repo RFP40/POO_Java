26b1022364a57b7e4e2d01d102cb4858704b6ef6
Rafael Mesquita Torres <rafamesto@gmail.com>
2026-03-18 17:22:35 -0300

    Aula04

---
+++ Aula04/Aula04.java
@@ -0,0 +1,11 @@
+package Aula04;
+public class Aula04 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("NIC", "Preta", 0.7f);
+        c1.status();
+
+        Caneta c2 = new Caneta("KKK", "Amarela", 3.0f);
+        c2.status();
+    }
+    
+}
